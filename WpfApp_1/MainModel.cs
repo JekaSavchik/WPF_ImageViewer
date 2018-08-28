b3dd3394789f7f348186cf4de3929a7222f126a2
Evgeniy <savchik.jeka@gmail.com>
2018-08-28 18:43:08 +0300

    Добавьте файлы проекта.

---
+++ WpfApp_1/MainModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfApp_1
+{
+    class MainModel
+    {
+        public ObservableCollection<string> Files { get; private set; }
+
+        public MainModel()
+        {
+            Files = new ObservableCollection<string>();
+        }
+
+        public void LoadFiles()
+        {
+            List<string> filter = new List<string>() { @"bmp", @"jpg", @"gif", @"png", @"tif" };
+            FileAttributes FAt;
+            FileInfo fileInfo;
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Files.Clear();
+                foreach (string file in Directory.GetFiles(dialog.SelectedPath))
+                {
+                    fileInfo = new FileInfo(file);
+                    FAt = fileInfo.Attributes;
+                    if ((FAt & FileAttributes.Hidden) != FileAttributes.Hidden)
+                        if (filter.Exists(n => n == file.Split(new char[] { '.' }).Last().ToLower()))
+                            Files.Add(file);
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp_1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void MenuItemOpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var model = new MainModel();
+            DataContext = model;
+            model.LoadFiles();
+        }            
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox lst = sender as ListBox;
+            string str = lst.SelectedItem as string;
+            if (str != null)
+            {
+                var bi = new BitmapImage(new Uri(str));
+                BigImage.Source = bi;
+            }
+        }
+
+        private void ButtonRight_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedIndex != -1)
+                listBox.SelectedIndex += 1;
+            else
+                listBox.SelectedIndex = 0;
+        }
+
+        private void ButtonLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedIndex != -1)
+                listBox.SelectedIndex -= 1;
+            else
+                listBox.SelectedIndex = 0;
+        }
+    }
+}
